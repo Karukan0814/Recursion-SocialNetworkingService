f814402c8860b02f99e50047bf704777c6ef2824
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-04-17 10:18:08 -0700

    NotFoundページレイアウト

--- front/src/pages/NotFoundPage.tsx
+++ front/src/pages/NotFoundPage.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Sidebar from "../component/Sidebar";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import "../style/NotFound.css";
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  //TODO　ログインチェック入れる（このページはログイン前提）
+
+  let navigate = useNavigate();
+
+  return (
+    <>
+      <Sidebar />
+      <div className="notfound_container">
+        <p className="notfound_text">
+          This page does not exist. Please search other words.
+        </p>
+        <Button
+          onClick={() => navigate("/search")}
+          className="jumpSearch__button"
+        >
+          Search
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default NotFoundPage;
