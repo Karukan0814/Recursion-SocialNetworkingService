c951642a187e1e559a9fa1c764d8b8a9d0aabb86
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-04-17 22:05:36 -0700

    Notificationページレイアウト追加

--- front/src/pages/NotificationPage.tsx
+++ front/src/pages/NotificationPage.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import Sidebar from "../component/Sidebar";
 import Widgets from "../component/Widgets";
 import NotificationCard from "../component/NotificationCard";
+import "../style/NotificationPage.css";
 
 const NotificationPage = () => {
   return (
     <>
       <Sidebar />
       <div className="notifications_container">
+        <div className="notificationsTitle__container">
+          <h2 className="notificationsTitle__text">Notifications</h2>
+        </div>
         {Array.from({ length: 5 }, (_, i) => i).map((number) => (
           <NotificationCard />
         ))}
