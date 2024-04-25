09fa0c91ace28aed21033f9248bd42679e0c324b
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-04-25 16:26:19 -0700

    jotaiprovider追加

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider as JotaiProvider } from "jotai";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <JotaiProvider>
+      <App />
+    </JotaiProvider>
   </React.StrictMode>
 );
