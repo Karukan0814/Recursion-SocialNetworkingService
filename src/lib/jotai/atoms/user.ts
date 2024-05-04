ef81c1ac9a401e7253cf4681cbb9596e6d3f4dce
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-05-03 20:59:06 -0700

    jotai persistance導入

--- src/lib/jotai/atoms/user.ts
+++ src/lib/jotai/atoms/user.ts
@@ -1,6 +1,28 @@
 import { atomWithStorage } from "jotai/utils";
 import { UserInfoType, UserInfoTypeJotai } from "../../type/UserInfoType";
+import { atom } from "jotai";
+
+const atomWithLocalStorage = (key: string, initialValue: UserInfoTypeJotai) => {
+  const getInitialValue = () => {
+    const item = localStorage.getItem(key);
+    if (item !== null) {
+      return JSON.parse(item);
+    }
+    return initialValue;
+  };
+  const baseAtom = atom(getInitialValue());
+  const derivedAtom = atom(
+    (get) => get(baseAtom),
+    (get, set, update) => {
+      const nextValue =
+        typeof update === "function" ? update(get(baseAtom)) : update;
+      set(baseAtom, nextValue);
+      localStorage.setItem(key, JSON.stringify(nextValue));
+    }
+  );
+  return derivedAtom;
+};
 
 export const jotaiInitialValue: UserInfoTypeJotai = {};
-const userInfoAtom = atomWithStorage("userInfo", jotaiInitialValue);
+const userInfoAtom = atomWithLocalStorage("userInfo", jotaiInitialValue);
 export { userInfoAtom };
