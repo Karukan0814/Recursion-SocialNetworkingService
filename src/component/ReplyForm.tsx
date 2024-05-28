b046f2b47c8ac6024dc6dfd9ad6bf60d29f9673f
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-05-27 22:40:01 -0700

    Replyformレイアウト崩れ修正

--- src/component/ReplyForm.tsx
+++ src/component/ReplyForm.tsx
@@ -5,6 +5,7 @@ import { Avatar } from "@mui/material";
 import "../style/ReplyForm.css";
 import PostBox from "./PostBox";
 import usePosts from "../hooks/usePosts";
+import { getMediaType } from "../lib/utils/utils";
 
 type Props = {
   post: PostInfo;
@@ -21,7 +22,53 @@ const ReplyForm = ({ post, handleClose, setReplyCount }: Props) => {
   }, [postList]);
   return (
     <div className="replyForm__container">
-      <Post post={post} displayFooter={false} />
+      <div className="replyToPost">
+        <div className="replyToPost__content">
+          <div className="replyToPost__header">
+            <div className="replyToPost__avatar">
+              <Avatar
+                src={post.user.userImg ?? "/assets/default_profile_400x400.png"}
+              />
+            </div>
+          </div>
+          <div className="replyToPost__body">
+            <div className="replyToPost__body__content">
+              <div className="replyToPost__body__header">
+                <div className="replyToPost__headerText">
+                  <h3>
+                    <span className="replyToPost__headerSpecial">
+                      {post.user.name}
+                    </span>
+                  </h3>
+                  <h3>
+                    <span className="replyToPost__headerSpecial time">
+                      {post.sentAt?.toLocaleString()}
+                    </span>
+                  </h3>
+                </div>
+              </div>
+              <div className="replyToPost__headerDescription">
+                <p>{post.text}</p>
+              </div>
+            </div>
+            {post.img && (
+              <div className="replyToPost__img__container">
+                {/* post.imgのurlの先にあるファイルが画像であればimg、videoであればvideo表示 */}
+                {getMediaType() === "image" && (
+                  <img
+                    src={post.img}
+                    alt={`${post.id}_photo_img`}
+                    className="replyToPost__img"
+                  />
+                )}
+                {getMediaType() === "video" && (
+                  <video controls src={post.img} className="replyToPost__img" />
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
       <PostBox
         registerPost={registerPost}
         registerLoading={loading}
