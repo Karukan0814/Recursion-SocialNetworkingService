2141a3522cf89d3c873a495e73cb870a642ea681
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-05-27 22:38:01 -0700

    不要ファイル削除

--- src/lib/utils/checkType.ts
+++
@@ -1,6 +0,0 @@
-import { articleTypeList } from "../constants";
-import { ArticleType } from "../type/ArticleInfoTypes";
-
-export function isArticleType(articleType: string): articleType is ArticleType {
-  return articleTypeList.includes(articleType);
-}
--- src/lib/utils/truncateText.ts
+++
@@ -1,6 +0,0 @@
-export function truncateText(text: string, maxWordLength: number) {
-  if (text.length > maxWordLength) {
-    return text.substring(0, maxWordLength) + "...";
-  }
-  return text;
-}
