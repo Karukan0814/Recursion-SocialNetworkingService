ffe8c30376a7df330fe69340638b3ce11c84ad28
Karukan0814 <46077409+Karukan0814@users.noreply.github.com>
2024-06-04 11:08:20 -0700

    Message画面レスポンシブ対応

--- src/component/Message.tsx
+++ src/component/Message.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../style/Message.css";
 
 import Conversation from "./Conversation";
 import ConversationList from "./ConversationList";
 import { ConversationInfoType } from "../lib/type/MessageInfoType";
+import { IconButton } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 type Props = { triggeredById?: number };
 
@@ -11,18 +13,69 @@ const Message = ({ triggeredById }: Props) => {
   const [activeConversationInfo, setActiveConversationInfo] =
     useState<ConversationInfoType>();
 
+  const [isScreenSmall, setIsScreenSmall] = useState(window.innerWidth <= 1066);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsScreenSmall(window.innerWidth <= 1066);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleBack = () => {
+    setActiveConversationInfo(undefined);
+  };
+
   return (
     <div className="message_container">
-      <div className="messageList_container">
-        <ConversationList
-          activeConversationInfo={activeConversationInfo}
-          setActiveConversationInfo={setActiveConversationInfo}
-          triggeredById={triggeredById}
-        />
-      </div>
-      <div className="sectionMessages_conversation">
-        <Conversation activeConversationInfo={activeConversationInfo} />
-      </div>
+      {isScreenSmall ? (
+        <>
+          {!activeConversationInfo ? (
+            <div
+              className={`messageList_container ${
+                isScreenSmall && "message__isSmall"
+              }`}
+            >
+              <ConversationList
+                activeConversationInfo={activeConversationInfo}
+                setActiveConversationInfo={setActiveConversationInfo}
+                triggeredById={triggeredById}
+              />
+            </div>
+          ) : (
+            <div
+              className={`sectionMessages_conversation ${
+                isScreenSmall && "message__isSmall"
+              }`}
+            >
+              <div className="sectionMessages_header">
+                <IconButton className="iconButton" onClick={handleBack}>
+                  <ArrowBackIcon />
+                </IconButton>
+              </div>
+              <Conversation activeConversationInfo={activeConversationInfo} />
+            </div>
+          )}
+        </>
+      ) : (
+        <>
+          <div className="messageList_container">
+            <ConversationList
+              activeConversationInfo={activeConversationInfo}
+              setActiveConversationInfo={setActiveConversationInfo}
+              triggeredById={triggeredById}
+            />
+          </div>
+
+          <div className="sectionMessages_conversation">
+            <Conversation activeConversationInfo={activeConversationInfo} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
